e758c9342f0b32bf21e09a71d2475ef7e15820a5
Egoushka <55855551+Egoushka@users.noreply.github.com>
2021-03-15 17:15:06 +0200

    nya

---
+++ PROLETARIESFABRIC/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace PROLETARIESFABRIC
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the URL of the person's profile");
+            string profileUrl = Console.ReadLine();
+            Console.WriteLine("Enter the path to the folder");
+            string savePath = Console.ReadLine();
+            var chromeOptions = new ChromeOptions();
+            int index = 0;
+            using (WebClient client = new WebClient())
+            {
+                using (var browser = new ChromeDriver(chromeOptions))
+                {
+                    browser.Navigate().GoToUrl(profileUrl);
+                    foreach (var item in browser.FindElementsByClassName("FFVAD"))
+                    {
+                        var photoUrl = item.GetAttribute("src");
+                        client.DownloadFile(new Uri(photoUrl), $@"{savePath}\" + index++ + ".png");
+                    }
+                }
+            }
+
+        }
+    }
+}
